b9359351f25fadc1ed7723ba06b60bcdf3492ea7
mohit_singh <40710129+scimohit66448@users.noreply.github.com>
2022-04-15 22:04:20 +0530

    Add files via upload
    Updated DB_URL through environment variable

--- Constants.py
+++ Constants.py
@@ -1,8 +1,4 @@
-conn_string = "host='localhost' dbname='restUser' user='postgres' password='mohit123'"
-DB_URL = "postgresql://postgres:mohit123@localhost:5432/restUser"
-
-
+# conn_string = "host='localhost' dbname='restUser' user='postgres' password='mohit123'"
+# DB_URL = "postgresql://postgres:mohit123@localhost:5432/restUser"
 import os
-
-db_url = os.getenv('DB_URL')
-print(db_url)
+DB_URL = os.environ['DB_URL']
