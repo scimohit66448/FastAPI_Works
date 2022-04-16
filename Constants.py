0941605a90febc75a97e351aa2a029fd7efcdd78
mohit_singh <40710129+scimohit66448@users.noreply.github.com>
2022-04-16 21:42:17 +0530

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,4 +1,3 @@
-# conn_string = "host='localhost' dbname='restUser' user='postgres' password='mohit123'"
-# DB_URL = "postgresql://postgres:mohit123@localhost:5432/restUser"
+
 import os
 DB_URL = os.environ['DB_URL']
